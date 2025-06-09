0aa50026e881cfe1f2313150654a67e6cd03a9ce
MahmoudAAbdelrahim <mahmoudalimahmoud536@gmail.com>
2025-06-10 02:34:44 +0300

    NotFound.jsx

---
+++ src/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="container text-center mt-5">
+      <h1 className="text-danger">404 - الصفحة غير موجودة</h1>
+      <p className="text-muted">   </p>
+    </div>
+  );
+};
+
+export default NotFound;
