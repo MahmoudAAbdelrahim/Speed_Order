30d093ee233c40037450f78f80d6d338dd78c1c8
MahmoudAAbdelrahim <mahmoudalimahmoud536@gmail.com>
2025-06-11 01:48:40 +0300

    register.js

--- src/api/register.js
+++ src/api/register.js
@@ -1,5 +1,3 @@
-// ملف: /api/register.js
-
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Only POST requests allowed." });
