046ed956358a391f4a39afc6f3444dac36e2951a
MahmoudAAbdelrahim <mahmoudalimahmoud536@gmail.com>
2025-06-15 09:35:02 +0300

    SignUp.jsx

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import { useNavigate, useLocation, Link } from "react-router-dom";
 
 const Signup = () => {
