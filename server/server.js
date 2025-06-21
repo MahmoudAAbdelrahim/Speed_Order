15ed0f1429278125a16616e5644100f214ee9c06
MahmoudAAbdelrahim <mahmoudalimahmoud536@gmail.com>
2025-06-21 03:00:25 +0300

    server.js

--- server/server.js
+++ server/server.js
@@ -40,5 +40,7 @@ mongoose.connect(process.env.MONGO_URI)
   });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
